85763c799125675b82ec5b30867f7f0888db15c3
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-02 17:45:51 -0300

    add function to sum up all negative values

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const transactionUl = document.querySelector("#transactions");
+const incomeDisplay = document.querySelector("#money-plus");
 
 const dummyTransactions = [
 	{id: 1, name:"Bolo de brigadeiro", amount:-20 },
 	{id: 2, name:"Salário", amount:300 },
 	{id: 3, name:"Torta de frango", amount: -10 },
 	{id: 4, name:"Violão", amount:150 }
-
 ]
-
 const addTransactionIntoDOM = transaction =>{
 	const operator = transaction.amount  < 0 ? "-" : "+";
 	const CSSClass = transaction.amount <0 ? "minus" : "plus";
 	const amountWithoutOperator = Math.abs(transaction.amount);
 	const li = document.createElement("li");
-
 	li.classList.add(CSSClass);
 	li.innerHTML= `
 		${transaction.name}<span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>`
-
+	
 		transactionUl.prepend(li);
 }
-
 const updateBalanceValues = () => {
 	const transactionsAmounts = dummyTransactions
 		.map(transaction => transaction.amount);
@@ -31,28 +28,16 @@ const updateBalanceValues = () => {
 		.filter( value => value > 0 )
 		.reduce((accumulator, value) => accumulator + value, 0) // obtendo o valor total das receitas
 		.toFixed(2);
-	console.log(income);
+	
+	const expense = transactionsAmounts.filter(value => value < 0) //obtendo valores negativo
+		.reduce((accumulator, value) => accumulator + value, 0) // somando os valores negativos
+		.toFixed(2);
+	console.log(expense);
 
 }
-
 
 const init =()=> {
 	dummyTransactions.forEach(addTransactionIntoDOM);
 	updateBalanceValues();
-
 }
-
 init();
-
-
-
-
-
-
-
-
-
-
-
-
-
